4675e5e202cf7de28eac4eb5390e3ffece04ab28
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-07-28 15:45:38 -0400

    Test class

---
+++ FunctionsTest/SLParentClass.h
@@ -0,0 +1,14 @@
+//
+//  SLParentClass.h
+//  FunctionsTest
+//
+//  Created by Sergey Lukaschuk on 2023-07-28.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLParentClass: NSObject
+
+@end
+
+
---
+++ FunctionsTest/SLParentClass.m
@@ -0,0 +1,12 @@
+//
+//  SLParentClass.m
+//  FunctionsTest
+//
+//  Created by Sergey Lukaschuk on 2023-07-28.
+//
+
+#import "SLParentClass.h"
+
+@implementation SLParentClass
+
+@end
